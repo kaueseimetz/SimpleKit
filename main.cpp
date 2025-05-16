01ff27e9983329a6d54659e74020c8eac760bfad
Kaue Seimetz <kaueseimetz@gmail.com>
2025-05-15 21:48:43 -0300

    some changes and addictions

--- main.cpp
+++ main.cpp
@@ -3,22 +3,52 @@
 using std::cout;
 using std::endl;
 using std::string;
+using std::cin;
 
-void menu();
+int GetMenuResponse();
+string strip(string &input);
 
 int main(){
-    menu();
 
     return 0;
 }
 
-void menu(){
-cout << "+===================================+" << endl;
-cout << "|             SimpleKit             |" << endl;
-cout << "+===================================+" << endl;
-cout << "|  1. Get System Info               |" << endl;
-cout << "|  2. Get Memory Status             |" << endl;
-cout << "|  3. Get Network Status            |" << endl;
-cout << "|  4. Get Current Timestamp         |" << endl;
-cout << "+===================================+" << endl;
+int GetMenuResponse(){
+    cout << "+===================================+" << endl;
+    cout << "|             SimpleKit             |" << endl;
+    cout << "+===================================+" << endl;
+    cout << "|  1. Get System Info               |" << endl;
+    cout << "|  2. Get Memory Status             |" << endl;
+    cout << "|  3. Get Network Status            |" << endl;
+    cout << "|  4. Get Current Timestamp         |" << endl;
+    cout << "|  0. Exit App                      |" << endl;
+    cout << "+===================================+" << endl;
+    cout << "\n";
+    cout << "What you gonna do? >" << endl;
+    string response;
+    std::cin >> response;
+
+    if (response == "1"){
+        return 1;
+    } else if (response == "2"){
+        return 2;
+    } else if (response == "3"){
+        return 3;
+    } else if (response == "4"){
+        return 4;
+    } else if (response == "0"){
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+string strip(string &input){
+    string newString;
+    for (int i = 0; i < input.length(); i++) {
+        if (input[i] != ' ') {
+            newString += input[i];
+        }
+    }
+    return newString;
 }
